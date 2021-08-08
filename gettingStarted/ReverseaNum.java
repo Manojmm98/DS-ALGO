c5d8d192cfd63dd4a5d9716aaca8d61ac95d7dfe
Manojmm98 <manojmishra98feb@gmail.com>
2021-08-09 00:33:08 +0530

    done

---
+++ gettingStarted/ReverseaNum.java
@@ -0,0 +1,16 @@
+import java.util.*;
+   
+   public class Main{
+   
+   public static void main(String[] args) {
+     
+     Scanner scn = new Scanner(System.in);
+     int n = scn.nextInt();
+     // if we divide a number by 10 then reminder are came , are reverse of given number
+     while(n>0){
+         int ans = n%10;
+         n=n/10;
+         System.out.println(ans);
+     }
+    }
+   }
