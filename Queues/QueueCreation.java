fe59245d01222925c088bcae439fe9e75a2cd88d
Manojmm98 <manojmishra98feb@gmail.com>
2021-08-19 01:37:59 +0530

    update

---
+++ Queues/QueueCreation.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main {
+    public static void main(String[]args){
+    Queue <Integer> qu = new ArrayDeque<Integer>();
+    qu.add(10);
+    qu.add(20);
+    qu.add(30);
+    qu.remove();
+    qu.peek();
+    System.out.println(qu);
+    
+}
--- Stack/StackCreation.java
+++ Stack/StackCreation.java
@@ -1,9 +1,11 @@
 import java.util.stack;
 public class StackCreation {
+    public static void main(String[]args){
     Stack<Integer> stk = new Stack<Integer>();
     stk.push(10);
     stk.push(20);
     stk.push(30);
     stk.peek();
     
+}
 }
