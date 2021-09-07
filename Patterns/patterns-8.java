5b135c87d3efb1f64350e7d7fa38c2f039635b18
Manojmm98 <manojmishra98feb@gmail.com>
2021-09-07 16:05:15 +0530

    update

--- Patterns/patterns-8.java
+++ Patterns/patterns-8.java
@@ -19,7 +19,7 @@ public class Main {
 
         // here we have to print the star in the place of 5 in row 5 and 3 in row 3 and 2 in row 2 and so on
         // so if we will make two loop then then when the sum of i and j values equal to n+1 then print star
-        // in row 0 i+j is 0+0=0 and n+1=
+        // in all row add the rows 1+n =1+5=6 , 2+4=6 , 3+3=6  then star will be at tht place when i+j==n+1
 
      int n = scn.nextInt(); 
       for(int i=1;i<=n;i++){
